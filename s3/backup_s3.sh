002a9c49d073690ac40d26e90adb84f5360ffec5
EC2 Default User <ec2-user@ip-172-31-23-77.ec2.internal>
2026-04-07 15:24:44 +0000

    feat: subida a S3

--- s3/backup_s3.sh
+++ s3/backup_s3.sh
@@ -22,3 +22,19 @@ fi
 ARCHIVO_RESPALDO="backup_$FECHA.tar.gz"
 echo "Comprimiendo el directorio $DIRECTORIO..."
 tar -czf "$ARCHIVO_RESPALDO" "$DIRECTORIO"
+
+# Generación de logs y subida a S3
+ARCHIVO_LOG="logs/backup.log"
+
+echo "[$(date)] Iniciando subida de $ARCHIVO_RESPALDO al bucket $BUCKET..." >> "$ARCHIVO_LOG"
+
+aws s3 cp "$ARCHIVO_RESPALDO" "s3://$BUCKET/"
+
+if [ $? -eq 0 ]; then
+    echo "[$(date)]  Respaldo exitoso en s3://$BUCKET/" >> "$ARCHIVO_LOG"
+    echo " Proceso completado con éxito."
+    rm "$ARCHIVO_RESPALDO" # Borramos el archivo local para ahorrar espacio
+else
+    echo "[$(date)] Error al subir a S3" >> "$ARCHIVO_LOG"
+    echo " Error crítico. Revisa el log."
+fi
